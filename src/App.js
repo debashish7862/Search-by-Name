7fd329fb8602185003be3d0d4610d2b137ea50f4
debashish sahu <debashish7862@gmail.com>
2023-04-07 10:34:24 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Card';
 import User from './Components/User.jsx';
+
+import Userbyname from './Components/Userbyname.jsx';
+
+
 function App() {
   return (
     <div>
-     <User/>
+ <Userbyname/>
       
     </div>
   );
